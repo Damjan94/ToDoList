4ef9405cebe9ea39b4fccc0b4d439747f748f498
damjan <damjandjordjevic1994@gmail.com>
2022-07-11 21:49:48 +0200

    Fixed a bug when rotating screen doesn't save the word that user typed

--- app/src/main/java/com/example/todolist/presentation/to_do_list/view/ToDoList.kt
+++ app/src/main/java/com/example/todolist/presentation/to_do_list/view/ToDoList.kt
@@ -4,7 +4,11 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -67,7 +71,7 @@ fun AddListAction(viewModel: ToDoListViewModel) {
         modifier = Modifier.fillMaxWidth(),
         horizontalAlignment = Alignment.CenterHorizontally
         ) {
-        var text by remember{ mutableStateOf("") }
+        var text by rememberSaveable { mutableStateOf("") }
 
         TextField(
             value = text,
